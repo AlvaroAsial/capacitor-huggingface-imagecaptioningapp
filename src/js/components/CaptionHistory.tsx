30a2e3d9f3be4ae45bbbcdce5975484514f17b91
AlvaroAsial <alvaro@asial.co.jp>
2024-02-26 14:27:52 +0900

    Adding caption history

--- src/js/components/CaptionHistory.tsx
+++ src/js/components/CaptionHistory.tsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { Grid, Typography, IconButton, Box, AppBar, Toolbar, Modal } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { decode } from "base64-arraybuffer";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 const CaptionHistory = ({ performSQLAction, onClose }) => {
 
     const [data, setData] = useState([]);
-        const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [confirm, setConfirm] = useState(false);
 
     const handleImageClick = (image) => {
         setSelectedImage(image);
@@ -16,26 +24,39 @@ const CaptionHistory = ({ performSQLAction, onClose }) => {
         setSelectedImage(null);
     };
 
-    useEffect(() => {
-        const loadData = async () => {
+    const deleteCaptions = () => {
+        const deleteData = async () => {
             await performSQLAction(async (db: SQLiteConnection | null) => {
-                const jsonData = JSON.parse(JSON.stringify(await db?.query(`SELECT * FROM pastCaptions`)));
-                const parsedData = jsonData.values.map(item => ({
-                    ...item,
-                    image: URL.createObjectURL(new Blob([new Uint8Array(decode(item.image))], {
-                        type: `image/${item.format}`,
-                    }))
-                }));
-                console.log(jsonData.values)
-                setData(parsedData.reverse())
+                await db?.query(`DELETE FROM pastCaptions`);
+                setData([]);
             });
         }
+        setConfirm(false);
+        deleteData();
+        loadData();       
+    };
+
+    const loadData = async () => {
+        await performSQLAction(async (db: SQLiteConnection | null) => {
+            const jsonData = JSON.parse(JSON.stringify(await db?.query(`SELECT * FROM pastCaptions`)));
+            const parsedData = jsonData.values.map(item => ({
+                ...item,
+                image: URL.createObjectURL(new Blob([new Uint8Array(decode(item.image))], {
+                    type: `image/${item.format}`,
+                }))
+            }));
+            console.log(jsonData.values)
+            setData(parsedData.reverse())
+        });
+    }
+
+    useEffect(() => {
         loadData();
     }, []);
 
     return (
         <div style={{ height: '100vh', overflowY: 'scroll' }}>
-            <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{ flexGrow: 1 }} className="toolbar">
                 <AppBar position="static" sx={{ bgcolor: '#333333' }}>
                     <Toolbar>
                         <IconButton
@@ -48,39 +69,36 @@ const CaptionHistory = ({ performSQLAction, onClose }) => {
                         >
                             <CloseIcon />
                         </IconButton>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            sx={{ mr: 2 }}
+                            onClick={() => setConfirm(true)}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
             </Box>
-            <Grid container spacing={2} justifyContent="center" alignItems="flex-start">
-                {data.map((item, index) => (
-                    <Grid item key={index} xs={6} style={{ display: 'flex', justifyContent: 'center' }}>
-                        <div
-                            onClick={() => handleImageClick(item)}
-                            style={{
-                                width: '100%',
-                                height: '0',
-                                paddingBottom: '100%', 
-                                position: 'relative',
-                                cursor: 'pointer',
-                                overflow: 'hidden',
-                            }}
-                        >
-                            <img
-                                src={item.image}
-                                alt={`Captioned Image ${index}`}
-                                style={{
-                                    position: 'absolute',
-                                    top: '0',
-                                    left: '0',
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover', 
-                                }}
-                            />
-                        </div>
-                    </Grid>
-                ))}
-            </Grid>
+            {data.length === 0 ? <h2 style={{ paddingTop: '10px' }} >No previous captions...</h2> :
+                <Grid container spacing={2} justifyContent="center" alignItems="flex-start" style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
+                    {data.map((item, index) => (
+                        <Grid item key={index} xs={6} style={{ display: 'flex', justifyContent: 'center' }}>
+                            <div
+                                onClick={() => handleImageClick(item)}
+                                className='gridImageContainer'
+                            >
+                                <img
+                                    src={item.image}
+                                    className='gridImage'
+                                    alt={`Captioned Image ${index}`}
+                                />
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid> }
             <Modal
                 open={Boolean(selectedImage)}
                 onClose={handleCloseModal}
@@ -89,13 +107,6 @@ const CaptionHistory = ({ performSQLAction, onClose }) => {
                 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
             >
                 <Box sx={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px' }}>
-                    <IconButton
-                        aria-label="close"
-                        onClick={handleCloseModal}
-                        sx={{ position: 'absolute', top: 0, left: 0, color: 'black' }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
                     {selectedImage && (
                         <>
                             <img
@@ -110,6 +121,25 @@ const CaptionHistory = ({ performSQLAction, onClose }) => {
                     )}
                 </Box>
             </Modal>
+            <Dialog
+                open={confirm}
+                onClose={() => null}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Deleting Caption History..."}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setConfirm(false)}>Cancel</Button>
+                    <Button onClick={deleteCaptions} autoFocus>Confirm</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
